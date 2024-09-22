ffea086087afaf3f450cc0ebb33ab7b99dd0ff08
amin <seyyed.amin.8809@gmail.com>
2024-09-22 20:23:41 +0330

    Add miangin-nomre.py

--- adad-aval.py
+++ adad-aval.py
@@ -5,7 +5,6 @@ for i in range(1 ,a + 1):
     for x in range(1,i +1):
         if i % x == 0:
             n += 1
-            print('i : ',i,'x : ',x)
     if n == 2:
         aval.append(i)
 
---
+++ miangin-nomre.py
@@ -0,0 +1,7 @@
+myNomre = [19.5,16.75,18,19,20,20,20,17.25,14,16,17]
+x = 0
+for i in myNomre:
+    x += i
+x = (x / len(myNomre))
+
+print(x)
