b5837720933ecf9e8ff690072d442ee69fddd9de
amin <seyyed.amin.8809@gmail.com>
2024-09-27 20:02:48 +0330

    change line 16 in kolatz-plus.py

--- kolatz-plus.py
+++ kolatz-plus.py
@@ -2,17 +2,20 @@ a = int(input('Enter a number : '))
 mylist = [a]
 n = 0
 for i in range(a*3):
-    n += 1
     if a == 1:
         break
 
-    if a % 2 != 0:
-        a = ((a *3) +1)
-        mylist.append(int(a))
-
-    if a % 2 == 0:
+    elif a % 2 == 0:
         a = (a / 2)
         mylist.append(int(a))
+
+    else:
+        a = ((a *3) +1)
+        mylist.append(int(a))
+        
+    n += 1
+
+        
 
 print('number of loop = ',n)
 print(mylist)
