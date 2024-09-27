654e11d4863ab9e62d8368c1ca80f7d488f671c7
amin <seyyed.amin.8809@gmail.com>
2024-09-27 21:08:50 +0330

    fix bug list1

--- 2-list.py
+++ 2-list.py
@@ -1,10 +1,15 @@
-list1 = [1,5,8,9,10,23]
-list2 = [2,5,9,8,18,32]
+list1 = [1,5,8,9,10,9]
+list2 = [2,5,9,8,18,9]
 list3 = []
 
 for index in list1:
+
     for index2 in list2:
-        if index == index2:
+
+        if index == index2 and not(index in list3):
+
             list3.append(index)
+
+            break
 
 print(list3)
