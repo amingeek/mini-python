ab8fd8ba309973ef0c9c9dc8e5bc786b8bb8d9b1
amin <seyyed.amin.8809@gmail.com>
2024-10-20 18:25:36 +0330

    Fix Create mosaolas and lozy !

--- create-mosalas.py
+++ create-mosalas.py
@@ -1,26 +1,23 @@
-n = int(input("Enter toll of lozi : "))  # input user
+n = int(input("Enter toll of Mosalas : "))  
 nstar = (2 * n) - 1
 mylist = []
-mystars = [[' ' for _ in range(nstar)] for _ in range(nstar)]  # 2D list of spaces
+mystars = [[' ' for _ in range(nstar)] for _ in range(nstar)]
 p = 0
 
-# ساختن لیست از اعداد برای تعداد ستاره‌ها در هر خط
 for k in range(1, n * 2):
     if k <= n:
         mylist.append(k)
 
-# چاپ اطلاعات برای دیباگ
+print("mystars = ", mystars)
 print("nstar = ", nstar)
 print('mylist = ', mylist)
 
-# پر کردن mystars با نقاط، طوری که بین آنها فاصله باشد
 for i in mylist:
-    middle = nstar // 2  # موقعیت وسط
-    start = middle - (i - 1)  # محاسبه اولین نقطه
+    middle = nstar // 2
+    start = middle - (i - 1)
     for j in range(i):
-        mystars[p][start + 2 * j] = '.'  # قرار دادن نقطه با فاصله
+        mystars[p][start + 2 * j] = '.'
     p += 1
 
-# چاپ لوزی نهایی
 for line in mystars:
     print("".join(line))
