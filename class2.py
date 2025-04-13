85b13449631cbf23e857bcd6c1b4b40a6c974206
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-13 16:36:31 +0330

    add new quiz class

---
+++ class2.py
@@ -0,0 +1,11 @@
+class ketab:
+    def __init__(self, name, number, nobat, author, s_n, amanat, user_name, gereftan, pasdadan):
+        self.name = name
+        self.number = number
+        self.nobat = nobat
+        self.author = author
+        self.serial_number = s_n
+        self.amanat = amanat
+        self.user_name = user_name
+        self.historical = [[gereftan, pasdadan]]
+        
