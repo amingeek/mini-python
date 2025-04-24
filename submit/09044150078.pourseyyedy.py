7ca8942b09b42e2d932cab5e46d82a8189a8967f
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-24 19:56:50 +0330

    wanna submit readfile1

--- submit/09044150078.pourseyyedy.py
+++ submit/09044150078.pourseyyedy.py
@@ -1,31 +1,87 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-    def moarefi(self):
-        return (f"Esme man {self.name} ast va {self.age} sale hastam")
-        
-class Programmer(Person):
-    def __init__(self, p_language, p_level, name, age):
-        super().__init__(name, age)
-        self.p_language = p_language
-        self.p_level = p_level
+def openfile(namef, modef):
+    with open(namef, modef) as data:
+        strings = data.readlines()
+        result = []
+        for i in strings:
+            result.append(i.split())
+        data.close()
+        return result
+
+def writefile(namef, modef, data):
+    with open(namef, modef) as outputs:
+        result = ""
+        for i in range(len(data)):
+            for k in range(len(data[i])):
+                result += data[i][k] + " "
+            result += "\n"
+        outputs.write(result)
+        outputs.close()
+
+def writefileSorted(namef, modef, data):
+    temp = 0
+    while True:
+        n = 0
+        for i in range(2, len(data)):
+            if data[i][0] < data[i-1][0]:
+                n += 1
+                temp = data[i-1]
+                data[i-1] = data[i]
+                data[i] = temp
+                temp = 0
+        if n == 0:
+            with open(namef, modef) as outputs:
+                result = ""
+                for i in range(len(data)):
+                    for k in range(len(data[i])):
+                        result += data[i][k] + " "
+                    result += "\n"
+                    
+                outputs.write(result)
+                outputs.close()
+                return data
+        n = 0
+
+def findByNumber(number, data):
+    for i in range(1,len(data)):
+        if data[i][0] == number:
+            return data[i]
+    return "404! not found"
+
+def findByName(name, data):
+    for i in range(1,len(data)):
+        if data[i][1] == name:
+            return data[i]
+    return "404! not found"
+
+def findByRotbe(rotbe, data):
+    cr = data[rotbe]
+    return (f"{cr[1]} ba shomare {cr[0]}")
     
-    def moarefi(self):
-        return (f"man {self.name} hastam va {self.age} sale hastam va ba zaban {self.p_language} kar mikonam va dar sathe {self.p_level} hastam.")
+    
+def main():
+    data = openfile("data.txt", "r")
+    writefile("outputs.txt", "w", data)
+    sortedlist = writefileSorted("outputs_sorted.txt", "w", data)
 
-class Manager(Person):
-    def __init__(self, name, age, m_level):
-        super().__init__(name, age)
-        self.m_level = m_level
-        
-    def moarefi(self):
-        return (f"Man {self.name} hastam va {self.age} sale hastam va sathe modiriati ye man {self.m_level} ast")
+    while True:
+        try:
+            i = int(input("Find by number or name or find rotbe choice options : \n1 : number\n2 : name\n3 : rotbe\n0 : exit\nEnter your choice : "))
+            if i == 0:
+                return 0
+            elif i == 1:
+                number = int(input("Enter number to find it : "))
+                print(findByNumber(f"{number}", data))
+                return 0
+            elif i == 2:
+                name = input("Enter name to find it : ")
+                print(findByName(name, data))
+                return 0
+            elif i == 3:
+                rotbe = int(input("Enter a rotbe you wannt it : "))
+                print(findByRotbe(rotbe, sortedlist))
+                return 0
 
-class Tech_manager(Manager, Programmer):
-    def __init__(self, name, age, m_level, p_level, p_language):
-        Manager.__init__(self, name, age, m_level)
-        Programmer.__init__(self, name, age, p_level, p_language)
-        
-    def moarefi(self):
-        return (f"Man {self.name} hastam va {self.age} sale hastam va dar barname nevisi dar sathe {self.p_level} hastam va ba zaban {self.p_language} hastam va dar modiriat dar sathe {self.m_level} hastam")
+        except:
+            print("Enter a valid option")
+
+main()
