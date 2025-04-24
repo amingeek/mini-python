fd66cab49d1727633d4f6f26a0229ca6dde0acdc
Seyyed Amin <seyyed.amin.8809@gmail.com>
2025-04-24 19:55:53 +0330

    end readfile1

--- readfile/readfile1.py
+++ readfile/readfile1.py
@@ -16,7 +16,31 @@ def writefile(namef, modef, data):
             result += "\n"
         outputs.write(result)
         outputs.close()
-        
+
+def writefileSorted(namef, modef, data):
+    temp = 0
+    while True:
+        n = 0
+        for i in range(2, len(data)):
+            if data[i][0] < data[i-1][0]:
+                n += 1
+                temp = data[i-1]
+                data[i-1] = data[i]
+                data[i] = temp
+                temp = 0
+        if n == 0:
+            with open(namef, modef) as outputs:
+                result = ""
+                for i in range(len(data)):
+                    for k in range(len(data[i])):
+                        result += data[i][k] + " "
+                    result += "\n"
+                    
+                outputs.write(result)
+                outputs.close()
+                return data
+        n = 0
+
 def findByNumber(number, data):
     for i in range(1,len(data)):
         if data[i][0] == number:
@@ -29,9 +53,15 @@ def findByName(name, data):
             return data[i]
     return "404! not found"
 
+def findByRotbe(rotbe, data):
+    cr = data[rotbe]
+    return (f"{cr[1]} ba shomare {cr[0]}")
+    
+    
 def main():
     data = openfile("data.txt", "r")
     writefile("outputs.txt", "w", data)
+    sortedlist = writefileSorted("outputs_sorted.txt", "w", data)
 
     while True:
         try:
@@ -46,10 +76,12 @@ def main():
                 name = input("Enter name to find it : ")
                 print(findByName(name, data))
                 return 0
+            elif i == 3:
+                rotbe = int(input("Enter a rotbe you wannt it : "))
+                print(findByRotbe(rotbe, sortedlist))
+                return 0
 
         except:
             print("Enter a valid option")
-
-    
 
 main()
